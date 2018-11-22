746223bd38cdba671c91784d3a349972be8d02d6
Pranjal Das <pranjalwit@gmail.com>
2018-11-21 18:10:20 -0800

    test commit

--- app/src/main/java/com/dev/r19/localservicefinder/ClientLogin.java
+++ app/src/main/java/com/dev/r19/localservicefinder/ClientLogin.java
@@ -1,13 +1,92 @@
 package com.dev.r19.localservicefinder;
 
+//package info.adroidhive.firebase;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ClientLogin extends AppCompatActivity {
+
+    public static final String TAG = "ClientLogin";
+    EditText Uemail1, Upassword1;
+    Button UlogIn1;
+    FirebaseAuth auth1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_login);
+
+        auth1 = FirebaseAuth.getInstance();
+        Button btn1 = (Button) findViewById(R.id.UlogIn);
+        Button btn2 = (Button) findViewById(R.id.button4);
+        Uemail1 = (EditText) findViewById(R.id.Uemail);
+        Upassword1 = (EditText) findViewById(R.id.Upassword);
+        UlogIn1 = (Button) findViewById(R.id.UlogIn);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClientLogin.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClientLogin.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        UlogIn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String Uemail = Uemail1.getText().toString().trim();
+                String Upassword = Upassword1.getText().toString().trim();
+
+                if(TextUtils.isEmpty(Uemail)) {
+                    Toast.makeText(ClientLogin.this, "No email id", Toast.LENGTH_LONG).show();
+                }
+
+                if(TextUtils.isEmpty(Upassword)) {
+                    Toast.makeText(ClientLogin.this, "No password", Toast.LENGTH_LONG).show();
+                }
+                auth1.signInWithEmailAndPassword(Uemail,Upassword)
+                        .addOnCompleteListener(ClientLogin.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if(!task.isSuccessful())    {
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(ClientLogin.this, "Authentication failed", Toast.LENGTH_LONG).show();
+                                }
+                                else {
+                                    Toast.makeText(ClientLogin.this, "success", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(ClientLogin.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+
+                            }
+                        });
+
+            }
+        });
     }
 }
--- app/src/main/java/com/dev/r19/localservicefinder/MainActivity.java
+++ app/src/main/java/com/dev/r19/localservicefinder/MainActivity.java
@@ -1,13 +1,36 @@
 package com.dev.r19.localservicefinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button asClient,asServiceProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get Two Buttons Continue as Client and as Service Provider and add action to it.
+        asClient = (Button)findViewById(R.id.AsClient);
+        asServiceProvider = (Button)findViewById(R.id.AsServiceProvider);
+        asClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ClientCredentials.class);
+                startActivity(intent);
+            }
+        });
+
+        asServiceProvider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ServiceCredentials.class);
+                startActivity(intent);
+            }
+        });
+        //  Button Function Ends Here
     }
 }
