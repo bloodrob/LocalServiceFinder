7ab292e8952622dfd0ac8b898874da1604713615
Pranjal Das <pranjalwit@gmail.com>
2018-11-21 17:02:53 -0800

    test commit

--- app/src/main/java/com/dev/r19/localservicefinder/ServiceCredentials.java
+++ app/src/main/java/com/dev/r19/localservicefinder/ServiceCredentials.java
@@ -3,11 +3,11 @@ package com.dev.r19.localservicefinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomePage extends AppCompatActivity {
+public class ServiceCredentials extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_service_credentials);
     }
 }
---
+++ app/src/main/java/com/dev/r19/localservicefinder/ServiceLogin.java
@@ -0,0 +1,13 @@
+package com.dev.r19.localservicefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_login);
+    }
+}
