229cb46c7837e8c1826bfec251074ad06f35960a
akash <akashbora122@gmail.com>
2018-12-04 19:55:23 -0800

    test commit

--- app/src/main/java/com/dev/r19/localservicefinder/SelectItemDetail.java
+++ app/src/main/java/com/dev/r19/localservicefinder/SelectItemDetail.java
@@ -1,9 +1,11 @@
 package com.dev.r19.localservicefinder;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -18,7 +20,7 @@ public class SelectItemDetail extends AppCompatActivity {
 
     TextView name, phone, address;
     FirebaseDatabase database;
-    DatabaseReference ref;
+    DatabaseReference ref,ref1;
     String passname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +35,53 @@ public class SelectItemDetail extends AppCompatActivity {
         address = (TextView)findViewById(R.id.spaddress);
 
         final String nameget = passname.toString().trim();
+        Coordinates point1 = new Coordinates();
 
         database = FirebaseDatabase.getInstance();
         ref = database.getReference("Service_Provider_info");
+        ref1 = database.getReference("Service_Provider_info").child("geoPoint");
+
+        ref1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                Coordinates newRes = dataSnapshot.getValue(Coordinates.class);
+                Location loc = new Location("");
+                loc.setLongitude(newRes.longitude);
+                loc.setLatitude(newRes.latitude);
+                Toast.makeText(SelectItemDetail.this, " location is "+loc.getLongitude()+","+loc.getLatitude(),Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
 
         ref.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Detailselectitem res = dataSnapshot.getValue(Detailselectitem.class);
+
+
 
                 if (res.SP_name.equals(nameget)) {
                     name.setText(res.SP_name);
