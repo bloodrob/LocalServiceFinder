0af58abee1cf5cae29fd8e51d6e488bfb24ef8ba
Pranjal Das <pranjalwit@gmail.com>
2018-12-04 21:26:15 -0800

    test commit

--- app/src/main/java/com/dev/r19/localservicefinder/ServiceInfoInsert.java
+++ app/src/main/java/com/dev/r19/localservicefinder/ServiceInfoInsert.java
@@ -111,15 +111,15 @@ public class ServiceInfoInsert extends AppCompatActivity {
 
         // Adding Items to the Spinner *** Pranjal Das
         List<String> namelist1 = new ArrayList<>();
-        namelist1.add("Doctor");
-        namelist1.add("Tutor");
-        namelist1.add("Pharmacy");
-        namelist1.add("Medical");
-        namelist1.add("Electrician");
-        namelist1.add("IT Professional");
+        namelist1.add(getString(R.string.Prof_Doctor));
+        namelist1.add(getString(R.string.Prof_Tutor));
+        namelist1.add(getString(R.string.Prof_Pharmacy));
+        namelist1.add(getString(R.string.Prof_Hospital));
+        namelist1.add(getString(R.string.Prof_Electrician));
+        namelist1.add(getString(R.string.Prof_IT_Professional));
         namelist1.add("Home Broker");
         namelist1.add("Land Dealer");
-        namelist1.add("Lawyer");
+        namelist1.add(getString(R.string.Prof_Lawyer));
         namelist1.add("Plumber");
         namelist1.add("Shopping Mall");
         namelist1.add("Co-operative Shop");
@@ -128,17 +128,17 @@ public class ServiceInfoInsert extends AppCompatActivity {
         namelist1.add("All in one Shop");
         namelist1.add("Veterinarian");
         namelist1.add("Banker");
-        namelist1.add("Insurance Agent");
-        namelist1.add("Car ShowRoom");
+        namelist1.add(getString(R.string.Prof_Insurance_Agent));
+        namelist1.add(getString(R.string.Prof_Car_ShowRoom));
         namelist1.add("Brand ShowRoom");
         namelist1.add("Tea Shop");
-        namelist1.add("Restaurant");
-        namelist1.add("Food Dhaba");
+        namelist1.add(getString(R.string.Prof_Restaurant));
+        namelist1.add(getString(R.string.Prof_Dhaba));
         namelist1.add("Wine Shop");
         namelist1.add("Drinking Bar");
         namelist1.add("Pub");
         namelist1.add("Factory");
-        namelist1.add("Local Transport Agency");
+        namelist1.add(getString(R.string.Prof_Local_Transport_Agency));
         namelist1.add("Mobile Shop");
         namelist1.add("Bike Garage");
         namelist1.add("Car Garage");
@@ -158,7 +158,6 @@ public class ServiceInfoInsert extends AppCompatActivity {
         namelist1.add("Police Man");
         namelist1.add("Consultancy Firm");
         namelist1.add("Property Dealer");
-        namelist1.add("Hospital");
         namelist1.add("HR Consultancy");
 
         // Adding Items to the Spinner *** Pranjal Das
