1c7ed48db3ce808cd461fb45850acd403d23fa27
Pranjal Das <pranjalwit@gmail.com>
2018-12-06 09:55:55 -0800

    test commit

--- app/src/main/java/com/dev/r19/localservicefinder/MainActivity.java
+++ app/src/main/java/com/dev/r19/localservicefinder/MainActivity.java
@@ -232,8 +232,6 @@ public class MainActivity extends AppCompatActivity {
         serviceEntry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-         //       Intent intent = new Intent(MainActivity.this, Navigation.class);
-           //     startActivity(intent);
             }
         });
 
