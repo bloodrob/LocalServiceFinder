1805c5761bc920a94b7ce859f7aa00557e2ead49
R19 <binrobwal@gmail.com>
2018-12-08 22:33:37 +0530

    test commit

--- app/src/main/java/com/dev/r19/localservicefinder/ServiceFeedback.java
+++ app/src/main/java/com/dev/r19/localservicefinder/ServiceFeedback.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -15,12 +18,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceFeedback extends AppCompatActivity {
 
     public static final String TAG =ServiceFeedbackModel.class.getSimpleName() ;
-    EditText name, email, sub, message;
+    EditText name, email, message;
+    Spinner sub;
     Button submit;
     String getid;
+    List<String> sublist;
+    String Subject;
 
     FirebaseDatabase database;
     DatabaseReference ref;
@@ -32,9 +41,31 @@ public class ServiceFeedback extends AppCompatActivity {
 
         name = (EditText)findViewById(R.id.spname);
         email = (EditText)findViewById(R.id.spemail);
-        sub = (EditText)findViewById(R.id.spsubject);
+        sub = (Spinner)findViewById(R.id.spsubject);
         message = (EditText)findViewById(R.id.spmessage);
         submit = (Button)findViewById(R.id.submitfeedback);
+        sublist = new ArrayList<>();
+
+        sublist.add("Compliment");
+        sublist.add("Comment");
+        sublist.add("Report");
+        sublist.add("Suggest");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,sublist);
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        sub.setAdapter(arrayAdapter);
+
+        sub.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Subject = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         database = FirebaseDatabase.getInstance();
         ref = database.getReference("Service_Provider_Feedback");
@@ -44,7 +75,7 @@ public class ServiceFeedback extends AppCompatActivity {
             public void onClick(View v) {
                 String SP_name = name.getText().toString().trim();
                 String SP_email = email.getText().toString().trim();
-                String Subject = sub.getText().toString().trim();
+
                 String Message = message.getText().toString().trim();
 
                 ServiceFeedbackEntry(SP_name, SP_email, Subject, Message);
